65424831653e894ee8545e8a755e73035f56a040
Renan Barbosa <renan.santos@mdh.gov.br>
2022-05-27 17:42:29 -0300

    initial commit calculadora

---
+++ Calculadora/main.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+
+class Calc:
+
+    def __init__(self):
+
+        self.window = Tk()
+        self.window.title('Calculadora')
+        self.window.resizable(False, False)
+
+
+        # Visor de números
+        self.screen_numbers = Entry(self.window, font='Arial 20 bold', bg='#172d36', fg='white')
+        self.screen_numbers.pack()
+
+
+        self.frame = Frame(self.window)
+        self.frame.pack()
+
+        # Adicionando os botões
+        self.button_1 = Button(self.frame, bg='orange', text='1', bd=0, font='Arial 20 bold',
+                               width=5, height=3)
+        self.button_1.grid(row=0, column=0)
+
+        self.window.mainloop()
+
+Calc()
