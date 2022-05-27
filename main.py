6118a79c4c575a519aa97fe62a535bcb6a68a4d9
Renan Barbosa <renan.santos@mdh.gov.br>
2022-05-27 09:36:51 -0300

    basic notepad

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import tkinter
 
+
+def NewFile():
+    print('Crie um novo arquivo')
+
+
+def SaveFile():
+    print('Salvo um novo arquivo')
+
+
+def Save_as():
+    print('Salvar como')
+
+
 # Gerar a janela
 window = tkinter.Tk()
 window.title('Notepad')
 window.geometry('1280x720')
 window.minsize(width=1280, height=720)
 
-#
-text_area = tkinter.Text(window, font='Arial 20 bold')
+# Criar uma área de texto
+text_area = tkinter.Text(window, font='Arial 20 bold', width=1280, height=720)
 text_area.pack()
 
+# Criar um menu de opções
 main_menu = tkinter.Menu(window)
-main_menu.add_cascade(label='File')
+
+## Criando um menu dentro de outro menu
+file_menu = tkinter.Menu(main_menu, tearoff=0)
+file_menu.add_command(label='New', command=NewFile)
+file_menu.add_command(label='Save', command=SaveFile)
+file_menu.add_command(label='Save as ...', command=Save_as)
+file_menu.add_command(label='Exit', command=window.quit)
+
+main_menu.add_cascade(label='File', menu=file_menu)
 window.config(menu=main_menu)
-# dssd
+
+# Cria um loop para deixar a janela aberta
 window.mainloop()
