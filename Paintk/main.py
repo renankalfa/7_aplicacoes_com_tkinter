f1bafed577281a23f3b0c805cfb472612fc68e1d
Renan Barbosa <renan.santos@mdh.gov.br>
2022-05-30 14:58:21 -0300

    firsts button and widgets

--- Paintk/main.py
+++ Paintk/main.py
@@ -10,12 +10,12 @@ class Paintk:
         self.window.minsize(width=1280, height=720)
         self.window.resizable(False, False)
 
-        self.img_line = PhotoImage(file='Ícones/line.png')
-        self.img_oval = PhotoImage(file='Ícones/oval.png')
-        self.img_eraser = PhotoImage(file='Ícones/eraser.png')
-        self.img_save = PhotoImage(file='Ícones/save.png')
-        self.img_square = PhotoImage(file='Ícones/square.png')
-        self.img_new = PhotoImage(file='Ícones/new.png')
+        self.img_line = PhotoImage(file="icons/line.png")
+        self.img_oval = PhotoImage(file='icons/oval.png')
+        self.img_eraser = PhotoImage(file='icons/eraser.png')
+        self.img_save = PhotoImage(file='icons/save.png')
+        self.img_square = PhotoImage(file='icons/square.png')
+        self.img_new = PhotoImage(file='icons/new.png')
 
         self.colors = ('black', 'gray', 'red', 'green', 'blue', 'purple', 'orange', 'white')
 
@@ -29,6 +29,9 @@ class Paintk:
             self.button_color = Button(self.bar_menu, bg=i, width=3, height=2,
                                        command=None, bd=0)
             self.button_color.pack(side='left')
+
+        self.button_line = Button(self.bar_menu, image=self.img_line)
+        self.button_line.pack(side='right')
 
         self.area_draw = Canvas(self.window, height=720)
         self.area_draw.pack(fill='both')
