88d5b588179fd5cf3f587a7ff2f68fe2116baf7a
Renan Barbosa <renan.santos@mdh.gov.br>
2022-06-01 16:32:30 -0300

    initial structure

--- Download de Vídeos/main.py
+++ Download de Vídeos/main.py
@@ -1,1 +1,21 @@
-s
+from tkinter import *
+
+
+class DownTk:
+
+    def __init__(self):
+
+        self.window = Tk()
+        self.window.title('Youtube Downloader')
+        self.window.resizable(False, False)
+        self.window.geometry('1280x720+300+200')
+
+        self.img_logo = PhotoImage(file='assets/logo.png')
+
+        self.frame = Frame(self.window, bg='#3b3b3b')
+        self.frame.pack()
+
+        self.window.mainloop()
+
+
+DownTk()
